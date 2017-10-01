a77ef4d992a9767fa70f65ad651e3acaef849596
Martin <mnestorov@documaster.no>
2017-10-01 19:08:54 +0300

    Added ScreenBuffer, that fills the terminal with random words on the rhs

--- type-fast.js
+++ type-fast.js
@@ -1,6 +1,47 @@
-// Require the lib, get a working terminal
 const term = require('terminal-kit').terminal;
 
-term('Hello World');
+const termkit = require('./node_modules/terminal-kit/lib/termkit.js');
 
-term.red('red');
+const screenBuffer = termkit.ScreenBuffer;
+
+const wordList = ['business', 'left', 'vague', 'shock', 'loaf', 'reply', 'bell',
+  'dapper', 'escape', 'heavenly', 'abashed', 'fair', 'scandalous', 'needless',
+  'momentous', 'puffy', 'wave', 'vanish', 'bath', 'hospitable', 'humor', 'crack',
+  'board', 'race', 'breath', 'minute', 'deer', 'draconian', 'next', 'perform',
+  'daffy', 'square', 'highfalutin', 'spiky', 'entertain', 'attract', 'heat',
+  'harsh', 'fumbling', 'jealous'];
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - (min + 1))) + min;
+}
+
+const screen = {};
+
+function init() {
+  termkit.getDetectedTerminal((error) => {
+    if (error) {
+      throw new Error('Cannot detect terminal!');
+    }
+
+    screen.words = screenBuffer.create({
+      dst: term,
+      width: Math.min(term.width),
+      height: Math.min(term.height - 1),
+    });
+  });
+}
+
+function fillScreen() {
+  screen.words.put({
+    x: 0, // getRandomInt(0, term.width / 2),
+    y: getRandomInt(0, term.height - 10),
+  }, wordList[getRandomInt(0, wordList.length)]);
+
+  screen.words.draw();
+}
+
+init();
+
+for (let i = 0; i < 10; i += 1) {
+  fillScreen();
+}
