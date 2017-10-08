acbeb17c3c8c09fea0ab5dd5e72f76e15d8a14e9
Martin (Anarcroth) <mdinestorov@gmail.com>
2017-10-08 18:46:00 +0300

    Added screen text file
    The screen text file now holds the methods and var about the screen with
    the text. It hadnles the hits with the player.

---
+++ src/screen-text.js
@@ -0,0 +1,123 @@
+const fs = require('fs');
+const player = require('./player.js');
+const termkit = require('terminal-kit');
+
+const wordList = fs.readFileSync('wordList.txt').toString().split('\n');
+
+const ScreenText = {
+  screenBuffer: termkit.ScreenBuffer,
+  searchedWord: '',
+  wordsYPos: [],
+  screen: {},
+
+  getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - (min + 1))) + min;
+  },
+
+  /* Put all of the words from the list on the right side of the screen
+  so that they do not jump immediately on the screen after the game satrts */
+  putWordsOffScreen() {
+    let xPos;
+    let yPos;
+
+    for (let word = 0; word < wordList.length; word += 1) {
+      xPos = this.getRandomInt(220, 816);
+      yPos = Math.floor(Math.random() * this.screen.text.height);
+
+      this.screen.text.put({
+        x: xPos,
+        y: yPos,
+      }, wordList[word]);
+
+      this.wordsYPos.push(yPos);
+    }
+  },
+
+  createScreenText(viewport) {
+    this.screen.text = this.screenBuffer.create({
+      width: viewport.width,
+      height: viewport.height,
+      noFill: true,
+    });
+
+    this.screen.text.fill({
+      attr: {
+        color: 'white', bgDefaultColor: true,
+      },
+    });
+  },
+
+  destroyMatchedWordAt(xp, yp) {
+    for (let ch = 0; ch < player.inpWord.length; ch += 1) {
+      delete this.screen.text.get({ x: xp + ch, y: yp });
+      this.screen.text.put({
+        x: xp + ch,
+        y: yp,
+      }, ' ');
+    }
+  },
+
+  optimizeOutterLoop(yp) {
+    // By splicing the index of the found Y position of the element, the
+    // number of iterrations is reduced each time a check for a amtch is made.
+    this.wordsYPos.splice(yp, 1);
+
+    // In order to break of the outter loop as well,
+    // make `yp` break its condition.
+    return this.wordsYPos.length;
+  },
+
+  isAPerfectMatch() {
+    return player.inpWord === this.searchedWord
+    && player.inpWord.length === this.searchedWord.length;
+  },
+
+  foundMatchOnScreen(viewport) {
+    for (let yp = this.screen.text.x > 0 ? this.screen.text.x - 10 : 0;
+      yp < this.wordsYPos.length; yp += 1) {
+      for (let xp = 0; xp < viewport.width; xp += 1) {
+        for (let ch = 0; ch < player.inpWord.length; ch += 1) {
+          // Optimized variant of the match: if the current box is null and the
+          // box, that is the length of the searched word away, also empty, jump
+          // with the length of the searched word forwad. Otherwise do a normal
+          // shift to the right.
+          if (this.screen.text.get({ x: xp + ch, y: yp }) === null &&
+              this.screen.text.get({ x: xp + player.inpWord.length, y: yp }) === null) {
+            xp += player.inpWord.length - 1;
+          } else {
+            this.searchedWord += this.screen.text.get({ x: xp + ch, y: yp }).char;
+          }
+        }
+
+        if (this.isAPerfectMatch()) {
+          player.incrHitStats();
+
+          this.destroyMatchedWordAt(xp, yp);
+
+          player.inpWord = '';
+
+          yp = this.optimizeOutterLoop(yp);
+
+          // Break inner loop.
+          // After tat breaks outter lopp automatically.
+          break;
+        }
+
+        this.searchedWord = '';
+      }
+    }
+  },
+
+  moveScreenTextLeft() {
+    this.screen.text.x -= 0.05;
+  },
+
+  draw(viewport) {
+    this.screen.text.draw({
+      dst: viewport,
+      tile: true,
+    });
+  },
+};
+
+module.exports = ScreenText;
--- src/type-fast.js
+++ src/type-fast.js
@@ -1,57 +1,13 @@
-const fs = require('fs');
 const player = require('./player.js');
 const termkit = require('terminal-kit');
+const screen = require('./screen-text.js');
 const cloptions = require('./cl-options.js');
 
 const term = termkit.terminal;
-const screenBuffer = termkit.ScreenBuffer;
 
-const wordList = fs.readFileSync('wordList.txt').toString().split('\n');
 const encouragement = ['Good!', 'Nice!', 'Doing great!', 'Awesome!', 'Keep it up!'];
 
-const wordsYPos = [];
-const screen = {};
-
 let viewport = {};
-
-let searchedWord = '';
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - (min + 1))) + min;
-}
-
-/* Put all of the words from the list on the right side of the screen
-so that they do not jump immediately on the screen after the game satrts */
-function putWordsOffScreen() {
-  let xPos;
-  let yPos;
-
-  for (let word = 0; word < wordList.length; word += 1) {
-    xPos = getRandomInt(220, 816);
-    yPos = Math.floor(Math.random() * screen.text.height);
-
-    screen.text.put({
-      x: xPos,
-      y: yPos,
-    }, wordList[word]);
-
-    wordsYPos.push(yPos);
-  }
-}
-
-function createScreenText() {
-  screen.text = screenBuffer.create({
-    width: viewport.width,
-    height: viewport.height,
-    noFill: true,
-  });
-
-  screen.text.fill({
-    attr: {
-      color: 'white', bgDefaultColor: true,
-    },
-  });
-}
 
 function terminate() {
   term.hideCursor(false);
@@ -66,69 +22,7 @@ function terminate() {
 
 function regHitOnScreen() {
   term.nextLine(1).eraseLine();
-  term.nextLine(2).cyan(`${encouragement[getRandomInt(0, encouragement.length)]}`).eraseLineAfter();
-}
-
-function destroyMatchedWordAt(xp, yp) {
-  for (let ch = 0; ch < player.inpWord.length; ch += 1) {
-    delete screen.text.get({ x: xp + ch, y: yp });
-    screen.text.put({
-      x: xp + ch,
-      y: yp,
-    }, ' ');
-  }
-}
-
-function optimizeOutterLoop(yp) {
-  // By splicing the index of the found Y position of the element, the
-  // number of iterrations is reduced each time a check for a amtch is made.
-  wordsYPos.splice(yp, 1);
-
-  // In order to break of the outter loop as well,
-  // make `yp` break its condition.
-  return wordsYPos.length;
-}
-
-function isAPerfectMatch() {
-  return player.inpWord === searchedWord && player.inpWord.length === searchedWord.length;
-}
-
-function searchScreenForMatch() {
-  for (let yp = screen.text.x > 0 ? screen.text.x - 10 : 0;
-    yp < wordsYPos.length; yp += 1) {
-    for (let xp = 0; xp < viewport.width; xp += 1) {
-      for (let ch = 0; ch < player.inpWord.length; ch += 1) {
-        // Optimized variant of the match: if the current box is null and the
-        // box, that is the length of the searched word away, also empty, jump
-        // with the length of the searched word forwad. Otherwise do a normal
-        // shift to the right.
-        if (screen.text.get({ x: xp + ch, y: yp }) === null &&
-            screen.text.get({ x: xp + player.inpWord.length, y: yp }) === null) {
-          xp += player.inpWord.length - 1;
-        } else {
-          searchedWord += screen.text.get({ x: xp + ch, y: yp }).char;
-        }
-      }
-
-      if (isAPerfectMatch()) {
-        player.incrHitStats();
-
-        regHitOnScreen();
-
-        destroyMatchedWordAt(xp, yp);
-
-        player.inpWord = '';
-
-        yp = optimizeOutterLoop(yp);
-
-        // Break inner loop.
-        // After tat breaks outter lopp automatically.
-        break;
-      }
-
-      searchedWord = '';
-    }
-  }
+  term.nextLine(2).cyan(`${encouragement[screen.getRandomInt(0, encouragement.length)]}`).eraseLineAfter();
 }
 
 function inpWord(key) {
@@ -155,8 +49,8 @@ function inpWord(key) {
     case ' ':
       // All of the words from the list are at least 5 chars long,
       // this avoids premature checking.
-      if (player.inpWord.length > 4) {
-        searchScreenForMatch();
+      if (player.inpWord.length > 4 && screen.foundMatchOnScreen(viewport)) {
+        regHitOnScreen();
       }
       break;
 
@@ -173,15 +67,15 @@ function init(callback) {
       throw new Error('Cannot detect terminal!');
     }
 
-    viewport = screenBuffer.create({
+    viewport = screen.screenBuffer.create({
       dst: term,
       width: Math.min(term.width) * 4,
       height: Math.min(term.height - 5),
       y: 2,
     });
 
-    createScreenText();
-    putWordsOffScreen();
+    screen.createScreenText(viewport);
+    screen.putWordsOffScreen();
 
     term.hideCursor();
     term.grabInput();
@@ -191,22 +85,14 @@ function init(callback) {
   });
 }
 
-function moveScreenTextLeft() {
-  screen.text.x -= 0.05;
-}
-
 function draw() {
-  screen.text.draw({
-    dst: viewport,
-    tile: true,
-  });
-
+  screen.draw(viewport);
   viewport.draw();
 }
 
 function animate() {
   draw();
-  moveScreenTextLeft();
+  screen.moveScreenTextLeft();
   setTimeout(animate, 10);
 }
 
